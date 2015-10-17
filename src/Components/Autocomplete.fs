a32a63310f4138f9a37e15477559e0a3014b8753
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-10-18 00:19:16 +0200

    Fix autocomplete length

--- src/Components/Autocomplete.fs
+++ src/Components/Autocomplete.fs
@@ -12,22 +12,34 @@ open DTO
 module Autocomplete =
     let private createProvider () =
         let provider = createEmpty<Modes.ISuggestSupport> ()
-        //provider.triggerCharacters <- [|"*"|]
-        //provider.excludeTokens <- [||]
         provider.suggest <- (fun doc pos _ ->
             LanguageService.completion (doc.getPath ()) (int pos.line) (int pos.character)
             |> Promise.success (fun o ->
                 o.Data |> Array.map (fun c ->
+                    let range = doc.getWordRangeAtPosition pos
+                    let length = range._end.character - range.start.character
                     let result = createEmpty<ISuggestions> ()
                     let sug = createEmpty<ISuggestion> ()
-                    sug._type <- c.Glyph
+                    sug._type <- c.Glyph.ToLower()
                     sug.label <- c.Name
                     sug.codeSnippet <- c.Name
                     result.currentWord <- c.Name
-                    result.suggestions <- [| sug |]
+                    result.suggestions <- [| sug; |]
+                    result.incomplete <- true
+                    result.overwriteBefore <- length
+                    result.overwriteAfter <- 0.
                     result)
                 )
             |> Promise.toThenable)
+        provider.getSuggestionDetails <- (fun doc pos sug _ ->
+            LanguageService.helptext sug.label
+            |> Promise.success (fun (o : HelptextResult) ->
+                let res = (o.Data.Overloads |> Array.fold (fun acc n -> (n |> Array.toList) @ acc ) []).Head.Signature
+                sug.documentationLabel <- res
+                sug
+                )
+            |> Promise.toThenable
+        )
         provider
 
     let activate (disposables: Disposable[]) =
