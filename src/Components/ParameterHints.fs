4b6156391679ee333eb4a2ab958a3f97a6704a26
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-03-14 14:57:23 +0100

    Add loging to ParameterHints provider

--- src/Components/ParameterHints.fs
+++ src/Components/ParameterHints.fs
@@ -17,26 +17,36 @@ module ParameterHints =
         let mapResult o =
             let res = createEmpty<SignatureHelp> ()
             let sigs = o.Data.Overloads |> Array.map (fun c ->
-                let signature = createEmpty<SignatureInformation> ()
-                let tip = c.Tip.[0].[0]
-                signature.label <-  tip.Signature
-                signature.documentation <- tip.Comment
-                signature.parameters <-
-                    c.Parameters |> Array.map (fun p ->
-                        let parameter = createEmpty<ParameterInformation> ()
-                        parameter.label <- p.Name
-                        parameter.documentation <- p.Description
-                        parameter )
-                signature )
+                try
+                    let signature = createEmpty<SignatureInformation> ()
+                    let tip = c.Tip.[0].[0]
+                    signature.label <-  tip.Signature
+                    signature.documentation <- tip.Comment
+                    signature.parameters <-
+                        c.Parameters |> Array.map (fun p ->
+                            let parameter = createEmpty<ParameterInformation> ()
+                            parameter.label <- p.Name
+                            parameter.documentation <- p.Description
+                            parameter )
+                    Some signature
+                with 
+                | e -> 
+                    Globals.console.error e
+                    None) |> Array.choose id
             res.activeParameter <- float (o.Data.CurrentParameter)
             res.activeSignature <- 0.
             res.signatures <- sigs
             res
 
+            
+        let logError (o : obj) = 
+            Globals.console.error o
+            null |> unbox<SignatureHelp>
+
         provider.``provideSignatureHelp <-`` (fun doc pos _ ->
             LanguageService.parse doc.fileName (doc.getText ())
             |> Promise.bind (fun _ -> LanguageService.methods (doc.fileName) (int pos.line + 1) (int pos.character + 1))
-            |> Promise.success mapResult
+            |> Promise.either mapResult logError
             |> Promise.toThenable )
         
         provider
