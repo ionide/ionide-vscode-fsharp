209b67d7aa565fd48f20ea6fc775c6b845657e56
oxxa <oxxa@users.noreply.github.com>
2016-07-21 23:56:34 +0100

    add config option to specify tools directory

--- src/Components/Environment.fs
+++ src/Components/Environment.fs
@@ -6,18 +6,21 @@ namespace Ionide.VSCode.FSharp
 // Below code adapted from similar in FSSutoComplete project
 module Environment =
     open Fable.Core
+    open Fable.Import.vscode
     open Fable.Import.Node
     open Fable.Import.Node.fs_types
 
-    let isWin = ``process``.platform = "win32"
+    let private isWin = ``process``.platform = "win32"
 
-    let (</>) a b =
+    let private (</>) a b =
         if isWin then a + @"\" + b
         else a + "/" + b
 
-    let dirExists dir = fs.statSync(dir).isDirectory()
+    let private dirExists dir = fs.statSync(dir).isDirectory()
 
-    let getOrElse defaultValue option =
+    let private fileExists file = fs.statSync(file).isFile()
+
+    let private getOrElse defaultValue option =
         match option with
         | None -> defaultValue
         | Some x -> x
@@ -32,11 +35,32 @@ module Environment =
             if detected = null then @"C:\Program Files (x86)\"
             else detected
 
-    let private fsharpInstallationPath () =
+    let private getToolsPathWindows () =
         [ "4.0"; "3.1"; "3.0" ]
         |> List.map (fun v -> programFilesX86 </> @"\Microsoft SDKs\F#\" </> v </> @"\Framework\v4.0")
         |> List.tryFind dirExists
 
+    let private getToolsPathFromConfiguration () =
+        let cfg = workspace.getConfiguration ()
+        let path = cfg.get("FSharp.toolsDirPath", "")
+        if not (path = "") && dirExists path then Some path
+        else None
+
+    let private getListDirectoriesToSearchForTools () =
+        if isWin then
+            [ getToolsPathFromConfiguration (); getToolsPathWindows () ] 
+        else
+            [ getToolsPathFromConfiguration () ]
+        |> List.choose id
+       
+    let private findFirstValidFilePath exeName directoryList =
+        directoryList
+        |> List.map (fun v -> v </> exeName)
+        |> List.tryFind fileExists    
+
     let fsi =
-        if not isWin then "fsharpi"
-        else getOrElse "" (fsharpInstallationPath ()) </> "fsi.exe"
+        let fileName = if isWin then "fsi.exe" else "fsharpi"
+        let dirs = getListDirectoriesToSearchForTools ()
+        match findFirstValidFilePath fileName dirs with
+        | None -> fileName
+        | Some x -> x
