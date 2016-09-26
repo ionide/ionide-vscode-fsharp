59acedc22535b3f97bb84ce74128b013dea9bd55
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-09-26 11:32:03 +0200

    Fix FSI (silentCd)

--- src/Components/Fsi.fs
+++ src/Components/Fsi.fs
@@ -11,11 +11,34 @@ open DTO
 open Ionide.VSCode.Helpers
 
 module Fsi =
+    [<Emit("setTimeout($0,$1)")>]
+    let setTimeout(cb, delay) : obj = failwith "JS Only"
+
     let mutable fsiOutput : Terminal option = None
 
     let isPowershell () =
         let t = workspace.getConfiguration().get("terminal.integrated.shell.windows", "")
         t.ToLower().Contains "powershell"
+
+    let sendCd () =
+        let editor = window.activeTextEditor
+        let file,dir =
+            if  JS.isDefined editor then
+                let file = editor.document.fileName
+                let dir = path.dirname file
+                file,dir
+            else
+                let dir = workspace.rootPath
+                path.join(dir, "tmp.fsx"), dir
+        let msg1 = sprintf "# silentCd @\"%s\";;\n" dir
+        let msg2 = (sprintf "# %d @\"%s\" \n" 1 file)
+
+
+        fsiOutput |> Option.iter (fun n ->
+            n.sendText(msg1, false)
+            n.sendText(msg2, false)
+            n.sendText(";;\n", false)
+        )
 
     let private start () =
         try
@@ -30,28 +53,21 @@ module Fsi =
                         sprintf "\"%s\" --fsi-server-input-codepage:65001" Environment.fsi, "cls"
                 else
                     Environment.fsi, "clear"
+
+
             terminal.sendText(clear, true)
             terminal.sendText(fsi, true)
+            setTimeout (sendCd, 1000) |> ignore
             terminal.show(true)
         with
         | _ ->
             window.showErrorMessage "Failed to spawn FSI, please ensure it's in PATH" |> ignore
 
 
-    let private send (msg : string) file =
+    let private send (msg : string) =
 
         if fsiOutput.IsNone then start ()
         let msg = msg + ";;\n"
-        let msg' =
-            try
-                let dir = path.dirname file
-                "\n"
-                + (sprintf "# silentCd @\"%s\" ;; " dir) + "\n"
-                + (sprintf "# %d @\"%s\" " 1 file) + "\n"
-                + msg
-            with
-            | _ -> msg
-
         fsiOutput |> Option.iter (fun fp -> fp.sendText(msg,false) )
 
 
@@ -60,7 +76,7 @@ module Fsi =
         let file = editor.document.fileName
         let pos = editor.selection.start
         let line = editor.document.lineAt pos
-        send line.text file
+        send line.text
         commands.executeCommand "cursorDown" |> ignore
 
     let private sendSelection () =
@@ -72,17 +88,19 @@ module Fsi =
         else
             let range = Range(editor.selection.anchor.line, editor.selection.anchor.character, editor.selection.active.line, editor.selection.active.character)
             let text = editor.document.getText range
-            send text file
+            send text
 
     let private sendFile () =
         let editor = window.activeTextEditor
         let file = editor.document.fileName
         let text = editor.document.getText ()
-        send text file
+        send text
 
 
 
     let activate (disposables: Disposable[]) =
+        window.onDidChangeActiveTextEditor $ ((fun n -> if JS.isDefined n then sendCd()), (), disposables) |> ignore
+
         commands.registerCommand("fsi.Start", start |> unbox) |> ignore
         commands.registerCommand("fsi.SendLine", sendLine |> unbox) |> ignore
         commands.registerCommand("fsi.SendSelection", sendSelection |> unbox) |> ignore
