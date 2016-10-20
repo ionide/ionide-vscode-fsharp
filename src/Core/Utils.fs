1744c468cd223efee32284b6509f73e1bc94e88e
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-10-20 14:28:32 +0300

    remove <summary> tags from tooltips

--- src/Core/Utils.fs
+++ src/Core/Utils.fs
@@ -95,19 +95,29 @@ module Array =
 module Markdown =
     open System.Text.RegularExpressions
     let private replacePatterns =
-        [ Regex "<c>(((?!<c>)(?!<\/c>).)*)<\/c>", fun x -> sprintf "`%s`" x ]
+        let r pat = Regex(pat, RegexOptions.Compiled ||| RegexOptions.IgnoreCase)
+        [ r"<c>(((?!<c>)(?!<\/c>).)*)<\/c>", sprintf "`%s`" ]
+
+    let private removePatterns =
+        [ "<summary>"; "</summary>" ]
 
     /// Replaces XML tags with Markdown equivalents.
     let replaceXml (str: string) : string =
-        replacePatterns
-        |> List.fold (fun res (regex: Regex, formatter: string -> string) ->
-            // repeat replacing with same pattern to handle nested tags, like `<c>..<c>..</c>..</c>`
-            let rec loop res : string =
-                match regex.Match res with
-                | m when m.Success -> loop <| res.Replace(m.Groups.[0].Value, formatter (m.Groups.[1].Value))
-                | _ -> res
-            loop res
-        ) str
+        let res =
+            replacePatterns
+            |> List.fold (fun res (regex: Regex, formatter: string -> string) ->
+                // repeat replacing with same pattern to handle nested tags, like `<c>..<c>..</c>..</c>`
+                let rec loop res : string =
+                    match regex.Match res with
+                    | m when m.Success -> loop <| res.Replace(m.Groups.[0].Value, formatter (m.Groups.[1].Value))
+                    | _ -> res
+                loop res
+            ) str
+        
+        removePatterns 
+        |> List.fold (fun (res: string) pat ->
+             res.Replace(pat, "")
+        ) res 
 
 module Promise =
     open Fable.Import.JS
