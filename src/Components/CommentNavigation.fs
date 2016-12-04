9246a85f03815d240590bf5197b21c11476aea74
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-12-04 17:31:25 +0100

    Navigate to symbol definition

--- src/Components/CommentNavigation.fs
+++ src/Components/CommentNavigation.fs
@@ -14,9 +14,6 @@ open Ionide.VSCode.Helpers
 
 module CommentNavigation =
 
-    let mutable file : string option = None
-    let mutable line : float option = None
-
 
     let private createProvider () =
 
@@ -27,41 +24,45 @@ module CommentNavigation =
                 |> String.split [|'\n'|]
                 |> Array.mapi (fun i r -> i,r)
                 |> Array.where (snd>>String.replace " " "" >> String.startsWith "//<code:")
-                |> Array.map (fun (i,r) -> i, String.split [|':'; '#' |] r, r)
-                |> Array.map (fun (i, res, l) ->
+                |> Array.map (fun (i, l) ->
+                    promise {
+                        let res = String.split [|':'; '#' |] l
+                        let startChar = l.IndexOf ":" |> float
+                        let endChar = l.IndexOf ">" |> float
+                        let dir = path.dirname doc.fileName
+                        let path = path.join(dir, res.[1])
+                        let location = res.[2] |> String.replace ">" ""
+                        let! line =
+                            if location |> float |> JS.isNaN |> not then
+                                location |> Promise.lift
+                            else
+                                promise {
+                                    let! td = workspace.openTextDocument path
+                                    let! _ = LanguageService.parse td.fileName (td.getText()) td.version
+                                    let! symbols = Symbols.getSymbols td
+                                    printfn "%A" symbols
+                                    let symOpt = symbols |> Array.tryFind (fun n -> n.name = location.Trim())
+                                    return
+                                        match symOpt with
+                                        | None -> "1"
+                                        | Some symbol ->
+                                            (symbol.location.range.start.line + 1.).ToString()
+                                }
+                        let uri = Uri.file (path + "#" + line)
+                        let l = float i
 
-                    let startChar = l.IndexOf ":" |> float
-                    let endChar = l.IndexOf ">" |> float
-                    let dir = path.dirname doc.fileName
-                    let path = path.join(dir, res.[1])
-                    line <- res.[2] |> String.replace ">" "" |> float |> Some
-                    file <- Some path
-                    let uri = Uri.file path
-                    let l = float i
-
-
-
-                    let range = vscode.Range(l,startChar + 1.,l,endChar)
-                    DocumentLink(range,uri))
-                |> Case1
+                        let range = vscode.Range(l,startChar + 1.,l,endChar)
+                        return DocumentLink(range,uri)
+                    })
+                |> Promise.all
+                |> Case2
             }
 
-    let navigate (n : TextEditor) =
-        let f = defaultArg file ""
-        let l = defaultArg line 0.
-        if n.document.fileName = f then
-            file <- None
-            line <- None
-            let range = vscode.Range(l,0.,l,100.)
-            n.revealRange(range, TextEditorRevealType.InCenter )
 
 
-
-        ()
 
     let activate selector (disposables: Disposable[]) =
         languages.registerDocumentLinkProvider (selector, createProvider()) |> ignore
-        window.onDidChangeActiveTextEditor $ (navigate, (), disposables) |> ignore
 
         ()
 
--- src/Components/Symbols.fs
+++ src/Components/Symbols.fs
@@ -13,46 +13,47 @@ open Ionide.VSCode.Helpers
 module Symbols =
 
 
-    let private createProvider () =
+    let private convertToKind code =
+        match code with
+        | "C" -> SymbolKind.Class     (*  CompletionItemKind.Class      *)
+        | "E" -> SymbolKind.Enum      (*  CompletionItemKind.Enum       *)
+        | "S" -> SymbolKind.Property      (*  CompletionItemKind.Value      *)
+        | "I" -> SymbolKind.Interface     (*  CompletionItemKind.Interface  *)
+        | "N" -> SymbolKind.Module      (*  CompletionItemKind.Module     *)
+        | "M" -> SymbolKind.Method     (*  CompletionItemKind.Method     *)
+        | "P" -> SymbolKind.Property      (*  CompletionItemKind.Property   *)
+        | "F" -> SymbolKind.Variable     (*  CompletionItemKind.Field      *)
+        | "T" -> SymbolKind.Class      (*  CompletionItemKind.Class      *)
+        | "Fc" -> SymbolKind.Function
+        | _   -> 0 |> unbox
 
-        let convertToKind code =
-            match code with
-            | "C" -> SymbolKind.Class     (*  CompletionItemKind.Class      *)
-            | "E" -> SymbolKind.Enum      (*  CompletionItemKind.Enum       *)
-            | "S" -> SymbolKind.Property      (*  CompletionItemKind.Value      *)
-            | "I" -> SymbolKind.Interface     (*  CompletionItemKind.Interface  *)
-            | "N" -> SymbolKind.Module      (*  CompletionItemKind.Module     *)
-            | "M" -> SymbolKind.Method     (*  CompletionItemKind.Method     *)
-            | "P" -> SymbolKind.Property      (*  CompletionItemKind.Property   *)
-            | "F" -> SymbolKind.Variable     (*  CompletionItemKind.Field      *)
-            | "T" -> SymbolKind.Class      (*  CompletionItemKind.Class      *)
-            | "Fc" -> SymbolKind.Function
-            | _   -> 0 |> unbox
-
-        let mapRes (doc : TextDocument) o =
-             if isNotNull o then
-                o.Data |> Array.map (fun syms ->
+    let private mapRes (doc : TextDocument) o =
+        if isNotNull o then
+            o.Data |> Array.map (fun syms ->
+                let oc = createEmpty<SymbolInformation>
+                oc.name <- syms.Declaration.Name
+                oc.kind <- syms.Declaration.GlyphChar |> convertToKind
+                oc.containerName <- syms.Declaration.Glyph
+                let loc = createEmpty<Location>
+                loc.range <- CodeRange.fromDTO syms.Declaration.BodyRange
+                loc.uri <- Uri.file doc.fileName
+                oc.location <- loc
+                let ocs =  syms.Nested |> Array.map (fun sym ->
                     let oc = createEmpty<SymbolInformation>
-                    oc.name <- syms.Declaration.Name
-                    oc.kind <- syms.Declaration.GlyphChar |> convertToKind
-                    oc.containerName <- syms.Declaration.Glyph
+                    oc.name <- sym.Name
+                    oc.kind <- sym.GlyphChar |> convertToKind
+                    oc.containerName <- sym.Glyph
                     let loc = createEmpty<Location>
-                    loc.range <- CodeRange.fromDTO syms.Declaration.BodyRange
+                    loc.range <- CodeRange.fromDTO sym.BodyRange
                     loc.uri <- Uri.file doc.fileName
                     oc.location <- loc
-                    let ocs =  syms.Nested |> Array.map (fun sym ->
-                        let oc = createEmpty<SymbolInformation>
-                        oc.name <- sym.Name
-                        oc.kind <- sym.GlyphChar |> convertToKind
-                        oc.containerName <- sym.Glyph
-                        let loc = createEmpty<Location>
-                        loc.range <- CodeRange.fromDTO sym.BodyRange
-                        loc.uri <- Uri.file doc.fileName
-                        oc.location <- loc
-                        oc )
-                    ocs |> Array.append (Array.create 1 oc)) |> Array.concat
-                else
-                    [||]
+                    oc )
+                ocs |> Array.append (Array.create 1 oc)) |> Array.concat
+            else
+                [||]
+
+
+    let private createProvider () =
 
         { new DocumentSymbolProvider
           with
@@ -68,3 +69,9 @@ module Symbols =
         languages.registerDocumentSymbolProvider(selector, createProvider())
         |> ignore
         ()
+
+    let getSymbols (doc : TextDocument) = promise {
+        let! o = LanguageService.declarations doc.fileName
+        return mapRes doc o
+    }
+
