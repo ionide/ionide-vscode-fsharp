54c08ba6edba11227540ebd06f18ff89fbc599d0
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-12-23 00:25:42 +0100

    Add newKeyword suggestion

--- src/Components/QuickFix.fs
+++ src/Components/QuickFix.fs
@@ -14,6 +14,32 @@ open Ionide.VSCode.Helpers
 
 module QuickFix =
 
+    let private getSuggestions doc (diagnostics : Diagnostic seq) =
+        let diagnostic = diagnostics |> Seq.tryFind (fun d -> d.message.Contains "Maybe you want one of the following:")
+        match diagnostic with
+        | None -> [||]
+        | Some d ->
+            d.message.Split('\n').[2..]
+            |> Array.map (fun suggestion ->
+                let s = suggestion.Trim()
+                let cmd = createEmpty<Command>
+                cmd.title <- sprintf "Replace with %s" s
+                cmd.command <- "fsharp.quickFix"
+                cmd.arguments <- Some ([| doc |> unbox; d.range |> unbox; s |> unbox; |] |> ResizeArray)
+                cmd
+            )
+    let private getNewKeywordSuggestions (doc : TextDocument) (diagnostics : Diagnostic seq) =
+        let diagnostic = diagnostics |> Seq.tryFind (fun d -> d.message.Contains "It is recommended that objects supporting the IDisposable interface are created using the syntax")
+        match diagnostic with
+        | None -> [||]
+        | Some d ->
+            let cmd = createEmpty<Command>
+            let s = "new " + doc.getText(d.range)
+            cmd.title <- sprintf "Add new"
+            cmd.command <- "fsharp.quickFix"
+            cmd.arguments <- Some ([| doc |> unbox; d.range |> unbox; s |> unbox; |] |> ResizeArray)
+            [| cmd |]
+
 
     let private createProvider () =
 
@@ -21,21 +47,10 @@ module QuickFix =
           with
             member this.provideCodeActions(doc, range, context, ct) =
                 let diagnostics = context.diagnostics
-                let diagnostic = diagnostics |> Seq.tryFind (fun d -> d.message.Contains "Maybe you want one of the following:")
-                let res =
-                    match diagnostic with
-                    | None -> [||]
-                    | Some d ->
-                        d.message.Split('\n').[2..]
-                        |> Array.map (fun suggestion ->
-                            let s = suggestion.Trim()
-                            let cmd = createEmpty<Command>
-                            cmd.title <- sprintf "Replace with %s" s
-                            cmd.command <- "fsharp.quickFix"
-                            cmd.arguments <- Some ([| doc |> unbox; d.range |> unbox; s |> unbox; |] |> ResizeArray)
-                            cmd
-                        )
-                res |> ResizeArray |> Case1
+                seq {
+                    yield! getSuggestions doc diagnostics
+                    yield! getNewKeywordSuggestions doc diagnostics
+                } |> ResizeArray |> Case1
             }
 
     let applyQuickFix(doc : TextDocument, range : vscode.Range, suggestion : string) =
