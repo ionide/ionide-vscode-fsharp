3dbd5f0c544bcec2a0a02529c4b7288a1e9ae5e8
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-12-27 18:16:36 +0100

    Add runSingle

--- src/Components/Expecto.fs
+++ src/Components/Expecto.fs
@@ -28,39 +28,79 @@ module Expecto =
 
 
     let private buildExpectoProjects () =
-
         outputChannel.clear ()
         if getConfig () then outputChannel.show ()
         getExpectoProjects ()
-        |> List.iter(fun proj ->
+        |> List.map(fun proj ->
             let path = proj.Project
             let msbuild = defaultArg Environment.msbuild "xbuild"
             Process.spawnWithNotification msbuild "" path outputChannel
-            |> Process.onExit(fun (code) ->
-                if code.ToString() <> "0" then
-                    vscode.window.showErrorMessage("Build of Expecto tests failed", "Show")
-                    |> Promise.map (fun n -> if n = "Show" then outputChannel.show () )
-                    |> ignore)
-            |> ignore
-        )
+            |> Process.toPromise)
+        |> Promise.all
+        |> Promise.bind (fun codes ->
+            if codes |> Seq.exists ((<>) "0") then
+                vscode.window.showErrorMessage("Build of Expecto tests failed", "Show")
+                |> Promise.map (fun n -> if n = "Show" then outputChannel.show () )
+                |> Promise.map (fun _ -> false)
+            else
+                Promise.lift true)
 
-    let private runExpecto () =
+    let private runExpecto args =
         outputChannel.clear ()
         if getConfig () then outputChannel.show ()
-        getExpectoExes ()
-        |> List.iter(fun exe ->
-            Process.spawnWithNotification exe "mono" "--summary" outputChannel
-            |> Process.onExit(fun (code) ->
-                if code.ToString() <> "0" then
-                    vscode.window.showErrorMessage("Expecto tests failed", "Show")
-                    |> Promise.map (fun n -> if n = "Show" then outputChannel.show () )
-                    |> ignore)
-            |> ignore )
+        buildExpectoProjects ()
+        |> Promise.bind (fun res ->
+            if res then
+                getExpectoExes ()
+                |> List.map(fun exe ->
+                    Process.spawnWithNotification exe "mono" args outputChannel
+                    |> Process.toPromise)
+                |> Promise.all
+                |> Promise.bind (fun codes ->
+                    if codes |> Seq.exists ((<>) "0") then
+                        vscode.window.showErrorMessage("Expecto tests failed", "Show")
+                        |> Promise.map (fun n -> if n = "Show" then outputChannel.show () )
+                    else
+                        Promise.empty)
+            else Promise.empty)
+
+    let private handleExpectoList (error : Error, stdout : Buffer, stderr : Buffer) =
+        if(stdout.toString() = "") then
+            [||]
+        else
+            stdout.toString().Split('\n')
+            |> Array.filter((<>) "")
+            |> Array.map (fun n -> n.Trim())
+
+    let private getList () =
+        buildExpectoProjects ()
+        |> Promise.bind (fun res ->
+            if res then
+                getExpectoExes ()
+                |> List.map (fun exe ->
+                    Process.exec exe "mono" "--list-tests"
+                )
+                |> Promise.all
+                |> Promise.map (Seq.collect (handleExpectoList) >> ResizeArray)
+            else
+                Promise.lift (ResizeArray()))
+
+    let private runAll () = runExpecto "--summary"
+
+    let private runSingle () =
+        window.showQuickPick (Case2 (getList() ))
+        |> Promise.bind(fun n ->
+            if JS.isDefined n then
+                (sprintf "--run \"%s\"" n).Replace("\n", "") |> runExpecto
+            else
+                Promise.empty
+        )
 
     let activate disposables =
-        let registerCommand com (f: unit->unit) =
+        let registerCommand com (f: unit-> _) =
             vscode.commands.registerCommand(com, unbox<Func<obj,obj>> f)
             |> ignore
 
         registerCommand "Expecto.build" buildExpectoProjects
-        registerCommand "Expecto.run" runExpecto
+        registerCommand "Expecto.run" runAll
+        registerCommand "Expecto.runSingle" runSingle
