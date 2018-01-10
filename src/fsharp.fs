49b5dbf30685d234fc44ba0b500db953399446ad
Maxime Mangel <maxime.shadaen@protonmail.com>
2018-01-10 16:04:04 +0100

    Add diagnostics commands (toggle mode + get infos)

--- src/fsharp.fs
+++ src/fsharp.fs
@@ -80,6 +80,7 @@ let activate (context: ExtensionContext) : Api =
         MSBuild.activate context
         SignatureData.activate context
         Debugger.activate context
+        Diagnostics.activate context
     )
     |> Promise.catch (fun error -> promise { () }) // prevent unhandled rejected promises
     |> ignore
