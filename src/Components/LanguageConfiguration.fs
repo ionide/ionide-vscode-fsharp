04ac79d31be64afe6348b72225e0dba656f38f35
Maxime Mangel <maxime.shadaen@protonmail.com>
2018-05-17 14:01:45 +0200

    Improve SignatureData generation + auto comment in doc comment section

---
+++ src/Components/LanguageConfiguration.fs
@@ -0,0 +1,31 @@
+namespace Ionide.VSCode.FSharp
+
+open Fable.Core.JsInterop
+open Fable.Import
+open Fable.Import.vscode
+open Ionide.VSCode.Helpers
+
+module LanguageConfiguration =
+
+    let activate (context: ExtensionContext) =
+        let config =
+            jsOptions<LanguageConfiguration> (fun o ->
+                o.onEnterRules <- Some <| ResizeArray<OnEnterRule>(
+                    [|
+                        // Doc single-line comment
+                        // Example: ///
+                        jsOptions<OnEnterRule> (fun rule ->
+                            rule.action <- jsOptions<EnterAction>(fun action ->
+                                action.indentAction <- IndentAction.Indent
+                                action.appendText <- Some "/// "
+                            )
+                            rule.beforeText <- JS.RegExp.Create("^\s*\/{3}.*$")
+                        )
+
+                    |]
+                )
+            )
+
+        JS.console.log config.onEnterRules.Value.[0].beforeText
+
+        context.subscriptions.Add(vscode.languages.setLanguageConfiguration("fsharp", config))
--- src/fsharp.fs
+++ src/fsharp.fs
@@ -87,6 +87,7 @@ let activate (context: ExtensionContext) : Api =
     Forge.activate context
     Fsi.activate context
     ScriptRunner.activate context
+    LanguageConfiguration.activate context
 
     let buildProject project = promise {
         let! exit = MSBuild.buildProjectPath "Build" project
