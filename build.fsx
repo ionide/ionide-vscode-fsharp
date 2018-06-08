e9c547cf9059572a7fb79f159ed4c69548ae0d3c
Gaston Cababie <nelak@users.noreply.github.com>
2018-06-08 17:54:52 -0300

    Updating to FAKE 5

--- build.fsx
+++ build.fsx
@@ -1,18 +1,44 @@
 // --------------------------------------------------------------------------------------
 // FAKE build script
 // --------------------------------------------------------------------------------------
+#if DOTNETCORE
+
+#r "paket:
+source nuget/dotnetcore
+source https://api.nuget.org/v3/index.json
+nuget Fake.Core.Target
+nuget Fake.IO.FileSystem
+nuget Fake.IO.Zip
+nuget Fake.JavaScript.Yarn
+nuget Fake.Core.ReleaseNotes
+nuget Fake.DotNet.Cli
+nuget Fake.Tools.Git
+nuget Octokit //"
+#endif
+
+#if DOTNETCORE
+// We need to use this for now as "regular" Fake breaks when its caching logic cannot find "intellisense.fsx".
+// This is the reason why we need to checkin the "intellisense.fsx" file for now...
+#load ".fake/build.fsx/intellisense.fsx"
+
+#else
 
 #I "packages/build/FAKE/tools"
 #r "FakeLib.dll"
+#r "packages/build/Octokit/lib/net45/Octokit.dll"
+
+#endif
+
 open System
-open System.Diagnostics
 open System.IO
-open Fake
-open Fake.Git
-open Fake.ProcessHelper
-open Fake.ReleaseNotesHelper
-open Fake.YarnHelper
-open Fake.ZipHelper
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.JavaScript
+open Fake.Tools
 
 
 // Git configuration (used for publishing documentation in gh-pages branch)
@@ -25,13 +51,13 @@ let gitHome = "https://github.com/" + gitOwner
 let gitName = "ionide-vscode-fsharp"
 
 // The url for the raw files hosted
-let gitRaw = environVarOrDefault "gitRaw" "https://raw.github.com/ionide"
+let gitRaw = Environment.environVarOrDefault "gitRaw" "https://raw.github.com/ionide"
 
 
 // Read additional information from the release notes document
 let releaseNotesData =
     File.ReadAllLines "RELEASE_NOTES.md"
-    |> parseAllReleaseNotes
+    |> ReleaseNotes.parseAll
 
 let release = List.head releaseNotesData
 
@@ -39,19 +65,23 @@ let msg =  release.Notes |> List.fold (fun r s -> r + s + "\n") ""
 let releaseMsg = (sprintf "Release %s\n" release.NugetVersion) + msg
 
 let run cmd args dir =
-    if execProcess( fun info ->
-        info.FileName <- cmd
-        if not( String.IsNullOrWhiteSpace dir) then
-            info.WorkingDirectory <- dir
-        info.Arguments <- args
-    ) System.TimeSpan.MaxValue = false then
+    if Process.execSimple( fun info -> 
+        { info with 
+            FileName = cmd
+            WorkingDirectory = if not( String.IsNullOrWhiteSpace dir) then
+                                dir 
+                                    else 
+                                ""
+            Arguments = args 
+        }
+    ) System.TimeSpan.MaxValue <> 0 then
         failwithf "Error while running '%s' with args: %s" cmd args
 
 
 let platformTool tool path =
-    match isUnix with
+    match Environment.isUnix with
     | true -> tool
-    | _ ->  match ProcessHelper.tryFindFileOnPath path with
+    | _ ->  match Process.tryFindFileOnPath path with
             | None -> failwithf "can't find tool %s on PATH" tool
             | Some v -> v
 
@@ -71,45 +101,48 @@ let fsacBinNetcore = fsacBin + "_netcore"
 // Build the Generator project and run it
 // --------------------------------------------------------------------------------------
 
-Target "Clean" (fun _ ->
-    CleanDir "./temp"
-    CopyFiles "release" ["README.md"; "LICENSE.md"]
-    CopyFile "release/CHANGELOG.md" "RELEASE_NOTES.md"
+Target.create "Clean" (fun _ ->
+    Shell.cleanDir "./temp"
+    Shell.copyFiles "release" ["README.md"; "LICENSE.md"]
+    Shell.copyFile "release/CHANGELOG.md" "RELEASE_NOTES.md"
 )
 
-Target "YarnInstall" <| fun () ->
-    Yarn (fun p -> { p with Command = Install Standard })
+Target.create "YarnInstall" (fun _ ->
+    Yarn.install (fun _ -> Yarn.defaultYarnParams))
 
-Target "DotNetRestore" <| fun () ->
-    DotNetCli.Restore (fun p -> { p with WorkingDir = "src" } )
+Target.create "DotNetRestore" (fun _ ->
+    let options = DotNet.Options.Create ()
+    DotNet.restore (fun p -> { p with Common = { options with WorkingDirectory = "src"} } ) |> ignore
+)
 
 let runFable additionalArgs noTimeout =
-    let cmd = "fable webpack -- --config webpack.config.js " + additionalArgs
+    let cmd = "fable webpack -- --config webpack.config.js"
+    //TODO: Remove timeout? Not available any more in DotNet exec
     let timeout = if noTimeout then TimeSpan.MaxValue else TimeSpan.FromMinutes 30.
-    DotNetCli.RunCommand (fun p -> { p with WorkingDir = "src"; TimeOut = timeout } ) cmd
+    DotNet.exec (fun p -> { p with WorkingDirectory = "src"; } ) cmd additionalArgs
 
-Target "RunScript" (fun _ ->
+Target.create "RunScript" (fun _ ->
     // Ideally we would want a production (minized) build but UglifyJS fail on PerMessageDeflate.js as it contains non-ES6 javascript.
-    runFable "" false
+    runFable "" false |> ignore
 )
 
-Target "Watch" (fun _ ->
-    runFable "--watch" true
+Target.create "Watch" (fun _ ->
+    runFable "--watch" true |> ignore
 )
 
-Target "CopyFSAC" (fun _ ->
-    ensureDirectory releaseBin
-    CleanDir releaseBin
+Target.create "CopyFSAC" (fun _ ->
+    Directory.ensure releaseBin
+    Shell.cleanDir releaseBin
 
     !! (fsacBin + "/*")
-    |> CopyFiles releaseBin
+    |> Shell.copyFiles releaseBin
 )
 
-Target "CopyFSACNetcore" (fun _ ->
-    ensureDirectory releaseBinNetcore
-    CleanDir releaseBinNetcore
+Target.create "CopyFSACNetcore" (fun _ ->
+    Directory.ensure releaseBinNetcore
+    Shell.cleanDir releaseBinNetcore
 
-    CopyDir releaseBinNetcore fsacBinNetcore (fun _ -> true)
+    Shell.copyDir releaseBinNetcore fsacBinNetcore (fun _ -> true)
 
     let mainfestFile = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
@@ -134,18 +167,18 @@ let forgeExe = "paket-files/github.com/fsharp-editing/Forge/temp/Forge.exe"
 let forgeConfig = "paket-files/github.com/fsharp-editing/Forge/temp/Forge.exe.config"
 
 
-Target "CopyForge" (fun _ ->
-    ensureDirectory releaseBinForge
-    ensureDirectory releaseForge
+Target.create "CopyForge" (fun _ ->
+    Directory.ensure releaseBinForge
+    Directory.ensure releaseForge
 
-    CleanDir releaseBinForge
-    checkFileExists forgeExe
+    Shell.cleanDir releaseBinForge
+    File.checkExists forgeExe
     !! forgeExe
     ++ forgeConfig
-    |> CopyFiles releaseForge
+    |> Shell.copyFiles releaseForge
 
     !! forgeBin
-    |> CopyFiles releaseBinForge
+    |> Shell.copyFiles releaseBinForge
 
 
 )
@@ -154,10 +187,10 @@ let fsgrammarDir = "paket-files/github.com/ionide/ionide-fsgrammar/grammar"
 let fsgrammarRelease = "release/syntaxes"
 
 
-Target "CopyGrammar" (fun _ ->
-    ensureDirectory fsgrammarRelease
-    CleanDir fsgrammarRelease
-    CopyFiles fsgrammarRelease [
+Target.create "CopyGrammar" (fun _ ->
+    Directory.ensure fsgrammarRelease
+    Shell.cleanDir fsgrammarRelease
+    Shell.copyFiles fsgrammarRelease [
         fsgrammarDir </> "fsharp.fsi.json"
         fsgrammarDir </> "fsharp.fsl.json"
         fsgrammarDir </> "fsharp.fsx.json"
@@ -169,20 +202,20 @@ Target "CopyGrammar" (fun _ ->
 let fsschemaDir = "schemas"
 
 let fsschemaRelease = "release/schemas"
-Target "CopySchemas" (fun _ ->
-    ensureDirectory fsschemaRelease
-    CleanDir fsschemaRelease
-    CopyFile fsschemaRelease (fsschemaDir </> "fableconfig.json")
-    CopyFile fsschemaRelease (fsschemaDir </> "wsconfig.json")
+Target.create "CopySchemas" (fun _ ->
+    Directory.ensure fsschemaRelease
+    Shell.cleanDir fsschemaRelease
+    Shell.copyFile fsschemaRelease (fsschemaDir </> "fableconfig.json")
+    Shell.copyFile fsschemaRelease (fsschemaDir </> "wsconfig.json")
 )
 
 
-Target "InstallVSCE" ( fun _ ->
-    killProcess "npm"
+Target.create "InstallVSCE" ( fun _ ->
+    Process.killAllByName "npm"
     run npmTool "install -g vsce" ""
 )
 
-Target "SetVersion" (fun _ ->
+Target.create "SetVersion" (fun _ ->
     let fileName = "./release/package.json"
     let lines =
         File.ReadAllLines fileName
@@ -194,21 +227,23 @@ Target "SetVersion" (fun _ ->
     File.WriteAllLines(fileName,lines)
 )
 
-Target "BuildPackage" ( fun _ ->
-    killProcess "vsce"
+Target.create "BuildPackage" ( fun _ ->
+    Process.killAllByName "vsce"
     run vsceTool.Value "package" "release"
     !! "release/*.vsix"
-    |> Seq.iter(MoveFile "./temp/")
+    |> Seq.iter(Shell.moveFile "./temp/")
 )
 
 
-Target "PublishToGallery" ( fun _ ->
+Target.create "PublishToGallery" ( fun _ ->
     let token =
-        match getBuildParam "vsce-token" with
+        //TODO: replaced with Environment.GetEnvironmentVariable as it seemed to be the best match
+        match Environment.GetEnvironmentVariable "vsce-token" with
         | s when not (String.IsNullOrWhiteSpace s) -> s
-        | _ -> getUserPassword "VSCE Token: "
+        //TODO: getUserPassword no longer available
+        | _ -> "" //getUserPassword "VSCE Token: "
 
-    killProcess "vsce"
+    Process.killAllByName "vsce"
     run vsceTool.Value (sprintf "publish --pat %s" token) "release"
 )
 
@@ -217,27 +252,31 @@ open Octokit
 
 
 
-Target "ReleaseGitHub" (fun _ ->
+Target.create "ReleaseGitHub" (fun _ ->
     let user =
-        match getBuildParam "github-user" with
+        //TODO: replaced with Environment.GetEnvironmentVariable as it seemed to be the best match
+        match Environment.GetEnvironmentVariable "github-user" with
         | s when not (String.IsNullOrWhiteSpace s) -> s
-        | _ -> getUserInput "Username: "
+        //TODO: getUserInput no longer available
+        | _ -> "" //getUserInput "Username: "
     let pw =
-        match getBuildParam "github-pw" with
+        //TODO: replaced with Environment.GetEnvironmentVariable as it seemed to be the best match
+        match Environment.GetEnvironmentVariable "github-pw" with
         | s when not (String.IsNullOrWhiteSpace s) -> s
-        | _ -> getUserPassword "Password: "
+        //TODO: getUserPassword no longer available
+        | _ -> "" //getUserPassword "Password: "
     let remote =
         Git.CommandHelper.getGitResult "" "remote -v"
         |> Seq.filter (fun (s: string) -> s.EndsWith("(push)"))
         |> Seq.tryFind (fun (s: string) -> s.Contains(gitOwner + "/" + gitName))
         |> function None -> gitHome + "/" + gitName | Some (s: string) -> s.Split().[0]
 
-    StageAll ""
-    Git.Commit.Commit "" (sprintf "Bump version to %s" release.NugetVersion)
-    Branches.pushBranch "" remote (Information.getBranchName "")
+    Git.Staging.stageAll ""
+    Git.Commit.exec "" (sprintf "Bump version to %s" release.NugetVersion)
+    Git.Branches.pushBranch "" remote (Git.Information.getBranchName "")
 
-    Branches.tag "" release.NugetVersion
-    Branches.pushTag "" remote release.NugetVersion
+    Git.Branches.tag "" release.NugetVersion
+    Git.Branches.pushTag "" remote release.NugetVersion
 
     let file = !! ("./temp" </> "*.vsix") |> Seq.head
 
@@ -253,9 +292,9 @@ Target "ReleaseGitHub" (fun _ ->
 // Run generator by default. Invoke 'build <Target>' to override
 // --------------------------------------------------------------------------------------
 
-Target "Default" DoNothing
-Target "Build" DoNothing
-Target "Release" DoNothing
+Target.create "Default" ignore
+Target.create "Build" ignore
+Target.create "Release" ignore
 
 "YarnInstall" ?=> "RunScript"
 "DotNetRestore" ?=> "RunScript"
@@ -281,7 +320,7 @@ Target "Release" DoNothing
 // ==> "InstallVSCE"
 ==> "BuildPackage"
 ==> "ReleaseGitHub"
-==> "PublishToGallery"
+//==> "PublishToGallery"
 ==> "Release"
 
-RunTargetOrDefault "Default"
+Target.runOrDefault "Default"
