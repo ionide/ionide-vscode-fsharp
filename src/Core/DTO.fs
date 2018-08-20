0ef53cdf7915ab64cc45418cff6e0f6b4410dc91
selketjah <gien@eightpointsquared.com>
2018-08-20 20:11:09 +0200

    refactor types in DTO file to F# guidelines

--- src/Core/DTO.fs
+++ src/Core/DTO.fs
@@ -2,292 +2,281 @@
 
 [<ReflectedDefinition>]
 module DTO =
-    type ParseRequest = { FileName : string; IsAsync : bool; Lines : string[]; Version : int }
-    type ProjectRequest = { FileName : string}
-    type DeclarationsRequest = {FileName : string; Lines : string[]; Version :int}
-    type HelptextRequest = {Symbol : string}
-    type PositionRequest = {FileName : string; Line : int; Column : int; Filter : string}
-    type CompletionRequest = {FileName : string; SourceLine : string; Line : int; Column : int; Filter : string; IncludeKeywords : bool; IncludeExternal: bool}
-    type WorkspacePeekRequest = {Directory: string; Deep: int; ExcludedDirs: string[] }
-    type WorkspaceLoadRequest = { Files: string[] }
 
-    type OverloadSignature = {
-        Signature: string
-        Comment: string
-    }
+    type ParseRequest = 
+        { FileName : string
+          IsAsync : bool
+          Lines : string[]
+          Version : int }
 
-    type TooltipSignature = {
-        Signature: string
-        Comment: string
-        Footer: string
-    }
+    type ProjectRequest = { FileName : string }
 
-    type DocumentationDescription = {
-        XmlKey: string
-        Constructors: string list
-        Fields: string list
-        Functions: string list
-        Signature: string
-        Comment: string
-        Footer: string
-    }
+    type DeclarationsRequest =
+        { FileName : string
+          Lines : string[]
+          Version : int }
+    type HelptextRequest = { Symbol : string }
 
+    type PositionRequest =
+        { FileName : string
+          Line : int
+          Column : int
+          Filter : string }
 
-    type Error = {
-        /// 1-indexed first line of the error block
-        StartLine : int
-        /// 1-indexed first column of the error block
-        StartColumn : int
-        /// 1-indexed last line of the error block
-        EndLine : int
-        /// 1-indexed last column of the error block
-        EndColumn : int
-        /// Description of the error
-        Message : string
-        ///Severity of the error - warning or error
-        Severity : string
-        /// Type of the Error
-        Subcategory : string
-        ///File Name
-        FileName : string
-        }
+    type CompletionRequest =
+        { FileName : string
+          SourceLine : string
+          Line : int
+          Column : int
+          Filter : string
+          IncludeKeywords : bool
+          IncludeExternal : bool }
 
-    type ErrorResp = {
-        File : string
-        Errors : Error []
-    }
+    type WorkspacePeekRequest =
+        { Directory : string
+          Deep : int
+          ExcludedDirs : string[] }
 
-    type Declaration = {
-        File : string
-        Line : int
-        Column : int
-    }
+    type WorkspaceLoadRequest = { Files : string[] }
 
-    type Completion = {
-        Name : string
-        ReplacementText : string
-        Glyph : string
-        GlyphChar: string
-        NamespaceToOpen: string
-    }
+    type OverloadSignature =
+        { Signature : string
+          Comment : string }
 
-    type SymbolUse = {
-      FileName : string
-      StartLine : int
-      StartColumn : int
-      EndLine : int
-      EndColumn : int
-      IsFromDefinition : bool
-      IsFromAttribute : bool
-      IsFromComputationExpression : bool
-      IsFromDispatchSlotImplementation : bool
-      IsFromPattern : bool
-      IsFromType : bool
-    }
+    type TooltipSignature =
+        { Signature : string
+          Comment : string
+          Footer : string }
 
-    type SymbolUses = {
-        Name : string
-        Uses : SymbolUse array
-    }
+    type DocumentationDescription =
+        { XmlKey : string
+          Constructors : string list
+          Fields : string list
+          Functions : string list
+          Signature : string
+          Comment : string
+          Footer : string }
 
-    type AdditionalEdit = {
-      Text: string
-      Line: int
-      Column: int
-    }
+    type Error =
+        { /// 1-indexed first line of the error block
+          StartLine : int
+          /// 1-indexed first column of the error block
+          StartColumn : int
+          /// 1-indexed last line of the error block
+          EndLine : int
+          /// 1-indexed last column of the error block
+          EndColumn : int
+          /// Description of the error
+          Message : string
+          ///Severity of the error - warning or error
+          Severity : string
+          /// Type of the Error
+          Subcategory : string
+          ///File Name
+          FileName : string }
 
-    type Helptext = {
-        Name : string
-        Overloads: OverloadSignature [] []
-        AdditionalEdit: AdditionalEdit
-    }
+    type ErrorResp =
+        { File : string
+          Errors : Error [] }
 
-    type OverloadParameter = {
-        Name : string
-        CanonicalTypeTextForSorting : string
-        Display : string
-        Description : string
-    }
-    type Overload = {
-        Tip : OverloadSignature [] []
-        TypeText : string
-        Parameters : OverloadParameter []
-        IsStaticArguments : bool
-    }
+    type Declaration =
+        { File : string
+          Line : int
+          Column : int }
 
-    type Method = {
-      Name : string
-      CurrentParameter : int
-      Overloads : Overload []
-    }
+    type Completion =
+        { Name : string
+          ReplacementText : string
+          Glyph : string
+          GlyphChar : string
+          NamespaceToOpen : string }
 
-    type CompilerLocation = {
-        Fsc : string
-        Fsi : string
-        MSBuild : string
-    }
+    type SymbolUse =
+        { FileName : string
+          StartLine : int
+          StartColumn : int
+          EndLine : int
+          EndColumn : int
+          IsFromDefinition : bool
+          IsFromAttribute : bool
+          IsFromComputationExpression : bool
+          IsFromDispatchSlotImplementation : bool
+          IsFromPattern : bool
+          IsFromType : bool }
 
-    type Range = {
-        StartColumn: int
-        StartLine: int
-        EndColumn: int
-        EndLine: int
-    }
+    type SymbolUses =
+        { Name : string
+          Uses : SymbolUse array }
 
-    type Pos = {
-        Line: int
-        Column: int
-    }
+    type AdditionalEdit =
+        { Text : string
+          Line : int
+          Column : int }
 
-    type Symbol = {
-        UniqueName: string
-        Name: string
-        Glyph: string
-        GlyphChar: string
-        IsTopLevel: bool
-        Range: Range
-        BodyRange : Range
-        File : string
-        EnclosingEntity : string
-        IsAbstract : bool
-    }
+    type Helptext =
+        { Name : string
+          Overloads : OverloadSignature [] []
+          AdditionalEdit : AdditionalEdit }
 
-    type Symbols = {
-        Declaration : Symbol;
-        Nested : Symbol []
-    }
+    type OverloadParameter =
+        { Name : string
+          CanonicalTypeTextForSorting : string
+          Display : string
+          Description : string }
 
-    type Fix = {
-        FromRange: Range
-        FromText: string
-        ToText: string
-    }
+    type Overload =
+        { Tip : OverloadSignature [] []
+          TypeText : string
+          Parameters : OverloadParameter []
+          IsStaticArguments : bool }
 
-    type Lint = {
-        Info : string
-        Input : string
-        Range : Range
-        Fix: Fix
-    }
+    type Method = 
+        { Name : string
+          CurrentParameter : int
+          Overloads : Overload [] }
+
+    type CompilerLocation =
+        { Fsc : string
+          Fsi : string
+          MSBuild : string }
+
+    type Range =
+        { StartColumn : int
+          StartLine : int
+          EndColumn : int
+          EndLine : int }
+
+    type Pos =
+        { Line : int
+          Column : int }
+
+    type Symbol =
+        { UniqueName : string
+          Name : string
+          Glyph : string
+          GlyphChar : string
+          IsTopLevel : bool
+          Range : Range
+          BodyRange : Range
+          File : string
+          EnclosingEntity : string
+          IsAbstract : bool }
+
+    type Symbols =
+        { Declaration : Symbol
+          Nested : Symbol [] }
+
+    type Fix =
+        { FromRange : Range
+          FromText : string
+          ToText : string }
+
+    type Lint =
+        { Info : string
+          Input : string
+          Range : Range
+          Fix : Fix }
 
     type ProjectFilePath = string
     type SourceFilePath = string
     type ProjectReferencePath = string
 
-    type ProjectLoading = {
-        Project: ProjectFilePath
-    }
+    type ProjectLoading = { Project : ProjectFilePath }
 
     [<RequireQualifiedAccess>]
     type ProjectResponseInfo =
-      | DotnetSdk of ProjectResponseInfoDotnetSdk
-      | Verbose
-      | ProjectJson
-    and ProjectResponseInfoDotnetSdk = {
-      IsTestProject: bool
-      Configuration: string
-      IsPackable: bool
-      TargetFramework: string
-      TargetFrameworkIdentifier: string
-      TargetFrameworkVersion: string
-      RestoreSuccess: bool
-      TargetFrameworks: string list
-      RunCmd: RunCmd option
-      IsPublishable: bool option }
-    and [<RequireQualifiedAccess>] RunCmd = { Command: string; Arguments: string }
+        | DotnetSdk of ProjectResponseInfoDotnetSdk
+        | Verbose
+        | ProjectJson
+    and ProjectResponseInfoDotnetSdk =
+        { IsTestProject : bool
+          Configuration : string
+          IsPackable : bool
+          TargetFramework : string
+          TargetFrameworkIdentifier : string
+          TargetFrameworkVersion : string
+          RestoreSuccess : bool
+          TargetFrameworks : string list
+          RunCmd : RunCmd option
+          IsPublishable : bool option }
+    and [<RequireQualifiedAccess>] RunCmd =
+        { Command : string
+          Arguments : string }
 
-    type Project = {
-        Project: ProjectFilePath
-        Files: SourceFilePath list
-        Output: string
-        References: ProjectReferencePath list
-        Logs: Map<string, string>
-        OutputType: string
-        Info: ProjectResponseInfo
-        AdditionalInfo: Map<string, string>
-    }
+    type Project =
+        { Project : ProjectFilePath
+          Files : SourceFilePath list
+          Output : string
+          References : ProjectReferencePath list
+          Logs : Map<string, string>
+          OutputType : string
+          Info : ProjectResponseInfo
+          AdditionalInfo : Map<string, string> }
 
-    type OpenNamespace = {
-        Namespace : string
-        Name : string
-        Type : string
-        Line : int
-        Column : int
-        MultipleNames : bool
-    }
+    type OpenNamespace =
+        { Namespace : string
+          Name : string
+          Type : string
+          Line : int
+          Column : int
+          MultipleNames : bool }
 
-    type QualifySymbol = {
-        Name : string
-        Qualifier : string
-    }
+    type QualifySymbol =
+        { Name : string
+          Qualifier : string }
 
-    type ResolveNamespace = {
-        Opens : OpenNamespace []
-        Qualifies: QualifySymbol []
-        Word : string
-    }
+    type ResolveNamespace =
+        { Opens : OpenNamespace []
+          Qualifies : QualifySymbol []
+          Word : string }
 
-    type UnionCaseGenerator = {
-        Text : string
-        Position : Pos
-    }
+    type UnionCaseGenerator =
+        { Text : string
+          Position : Pos }
 
-    type RecordStubCaseGenerator = {
-        Text : string
-        Position : Pos
-    }
+    type RecordStubCaseGenerator =
+        { Text : string
+          Position : Pos }
 
-    type Parameter = {
-        Name : string
-        Type : string
-    }
+    type Parameter =
+        { Name : string
+          Type : string }
 
-    type SignatureData = {
-        OutputType : string
-        Parameters : Parameter list list
-    }
+    type SignatureData =
+        { OutputType : string
+          Parameters : Parameter list list }
 
-    type WorkspacePeek = {
-        Found : WorkspacePeekFound []
-    }
+    type WorkspacePeek = { Found : WorkspacePeekFound [] }
     and WorkspacePeekFound =
-      | Directory of WorkspacePeekFoundDirectory
-      | Solution of WorkspacePeekFoundSolution
-    and WorkspacePeekFoundDirectory = {
-        Directory: string
-        Fsprojs: string []
-    }
-    and WorkspacePeekFoundSolution = {
-        Path: string
-        Items: WorkspacePeekFoundSolutionItem []
-        Configurations: WorkspacePeekFoundSolutionConfiguration []
-    }
-    and [<RequireQualifiedAccess>] WorkspacePeekFoundSolutionItem = {
-        Guid: string
-        Name: string
-        Kind: WorkspacePeekFoundSolutionItemKind
-    }
+        | Directory of WorkspacePeekFoundDirectory
+        | Solution of WorkspacePeekFoundSolution
+    and WorkspacePeekFoundDirectory =
+        { Directory : string
+          Fsprojs : string [] }
+    and WorkspacePeekFoundSolution =
+        { Path : string
+          Items : WorkspacePeekFoundSolutionItem []
+          Configurations : WorkspacePeekFoundSolutionConfiguration [] }
+    and [<RequireQualifiedAccess>] WorkspacePeekFoundSolutionItem =
+        { Guid : string
+          Name : string
+          Kind : WorkspacePeekFoundSolutionItemKind }
     and WorkspacePeekFoundSolutionItemKind =
         | MsbuildFormat of WorkspacePeekFoundSolutionItemKindMsbuildFormat
         | Folder of WorkspacePeekFoundSolutionItemKindFolder
-    and [<RequireQualifiedAccess>] WorkspacePeekFoundSolutionItemKindMsbuildFormat = {
-        Configurations: WorkspacePeekFoundSolutionConfiguration []
-    }
-    and [<RequireQualifiedAccess>] WorkspacePeekFoundSolutionItemKindFolder = {
-        Items: WorkspacePeekFoundSolutionItem []
-        Files: string []
-    }
-    and [<RequireQualifiedAccess>] WorkspacePeekFoundSolutionConfiguration = {
-        Id: string
-        ConfigurationName: string
-        PlatformName: string
-    }
+    and [<RequireQualifiedAccess>] WorkspacePeekFoundSolutionItemKindMsbuildFormat = { Configurations: WorkspacePeekFoundSolutionConfiguration [] }
+    and [<RequireQualifiedAccess>] WorkspacePeekFoundSolutionItemKindFolder =
+        { Items : WorkspacePeekFoundSolutionItem []
+          Files : string [] }
+    and [<RequireQualifiedAccess>] WorkspacePeekFoundSolutionConfiguration =
+        { Id : string
+          ConfigurationName : string
+          PlatformName : string }
 
-    type ResponseError<'T> = {
-        Code: int
-        Message: string
-        AdditionalData: 'T
-    }
+    type ResponseError<'T> =
+        { Code : int
+          Message : string
+          AdditionalData : 'T }
 
     [<RequireQualifiedAccess>]
     type ErrorCodes =
@@ -296,8 +285,8 @@ module DTO =
        | ProjectParsingFailed = 101
 
     module ErrorDataTypes =
-        type ProjectNotRestoredData = { Project: ProjectFilePath }
-        type ProjectParsingFailedData = { Project: ProjectFilePath }
+        type ProjectNotRestoredData = { Project : ProjectFilePath }
+        type ProjectParsingFailedData = { Project : ProjectFilePath }
 
     [<RequireQualifiedAccess>]
     type ErrorData =
@@ -305,33 +294,26 @@ module DTO =
        | ProjectNotRestored of ErrorDataTypes.ProjectNotRestoredData
        | ProjectParsingFailed of ErrorDataTypes.ProjectParsingFailedData
 
-    type UnusedDeclaration = {
-        Range: Range
-        IsThisMember: bool
-    }
+    type UnusedDeclaration =
+        { Range : Range
+          IsThisMember : bool }
 
-    type UnusedDeclarations = {
-        Declarations : UnusedDeclaration []
-    }
+    type UnusedDeclarations = { Declarations : UnusedDeclaration [] }
 
-    type UnusedOpens = {
-        Declarations : Range []
-    }
+    type UnusedOpens = { Declarations : Range [] }
 
-    type SimplifiedNameData = {
-        RelativeName : string
-        UnnecessaryRange: Range
-    }
+    type SimplifiedNameData =
+        { RelativeName : string
+          UnnecessaryRange : Range }
 
-    type SimplifiedName = {
-        Names: SimplifiedNameData []
-    }
+    type SimplifiedName = { Names: SimplifiedNameData [] }
 
-    type CompileData = {
-        Code : int
-    }
+    type CompileData = { Code : int }
 
-    type Result<'T> = {Kind : string; Data : 'T}
+    type Result<'T> =
+        { Kind : string
+          Data : 'T }
+
     type CompilerLocationResult = Result<CompilerLocation>
     type HelptextResult = Result<Helptext>
     type CompletionResult = Result<Completion[]>
