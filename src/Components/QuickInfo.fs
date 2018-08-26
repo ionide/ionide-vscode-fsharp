0cf32e275ac08f333a51d0e5d5e64ee2ff43af18
selketjah <gien@eightpointsquared.com>
2018-08-26 15:47:16 +0200

    clean up function declarations for quickinfo file, create namespace on top of file and add module quickfix

--- src/Components/QuickInfo.fs
+++ src/Components/QuickInfo.fs
@@ -1,83 +1,88 @@
-module Ionide.VSCode.FSharp.QuickInfo
+namespace Ionide.VSCode.FSharp
 
 open Fable.Import
 open Fable.Import.vscode
 open Ionide.VSCode.Helpers
 
-module private StatusDisplay =
-    let mutable private item : StatusBarItem option = None
-    let private hideItem () = item |> Option.iter (fun n -> n.hide ())
+module QuickInfo =
 
-    let private showItem (text: string) tooltip =
-        item.Value.text <- text
-        item.Value.tooltip <- tooltip
-        item.Value.show()
+    module private StatusDisplay =
 
-    let activate (context: ExtensionContext) =
-        item <- Some (window.createStatusBarItem (unbox 1, -10. ))
-        context.subscriptions.Add(item.Value)
+        let mutable private item : StatusBarItem option = None
+        let private hideItem () = item |> Option.iter (fun n -> n.hide ())
 
-    let private isFsharpTextEditor (textEditor : TextEditor) =
-        if JS.isDefined textEditor && JS.isDefined textEditor.document then
-            let doc = textEditor.document
-            match doc with
-            | Document.FSharp -> true
-            | _ -> false
-        else
-            false
+        let private showItem (text : string) tooltip =
+            item.Value.text <- text
+            item.Value.tooltip <- tooltip
+            item.Value.show()
 
-    let private getOverloadSignature (textEditor : TextEditor) (selections: ResizeArray<Selection>) = promise {
-        if isFsharpTextEditor textEditor && selections.Count > 0 then
-            let doc = textEditor.document
-            let pos = selections.[0].active
-            let! o = LanguageService.signature (doc.fileName) (int pos.line + 1) (int pos.character + 1)
-            if isNotNull o then
-                return Some o.Data
+        let activate (context : ExtensionContext) =
+            item <- Some (window.createStatusBarItem (unbox 1, -10. ))
+            context.subscriptions.Add(item.Value)
+
+        let private isFsharpTextEditor (textEditor : TextEditor) =
+            if JS.isDefined textEditor && JS.isDefined textEditor.document then
+                let doc = textEditor.document
+                match doc with
+                | Document.FSharp -> true
+                | _ -> false
             else
-                return None
-        else
-            return None
-    }
+                false
 
-    let update (textEditor : TextEditor) (selections: ResizeArray<Selection>) =
-        promise {
-            let! signature = getOverloadSignature textEditor selections
-            match signature with
-            | Some signature ->
-                showItem signature signature
-            | _ ->
-                hideItem()
-        } |> ignore
+        let private getOverloadSignature (textEditor : TextEditor) (selections : ResizeArray<Selection>) =
+            promise {
+                if isFsharpTextEditor textEditor && selections.Count > 0 then
+                    let doc = textEditor.document
+                    let pos = selections.[0].active
+                    let! o = LanguageService.signature (doc.fileName) (int pos.line + 1) (int pos.character + 1)
+                    if isNotNull o then
+                        return Some o.Data
+                    else
+                        return None
+                else
+                    return None
+            }
 
-    let clear () =
-        update JS.undefined (ResizeArray())
+        let update (textEditor : TextEditor) (selections : ResizeArray<Selection>) =
+            promise {
+                let! signature = getOverloadSignature textEditor selections
+                match signature with
+                | Some signature ->
+                    showItem signature signature
+                | _ ->
+                    hideItem()
+            } |> ignore
 
-let mutable private timer = None
-let private clearTimer () =
-    match timer with
-    | Some t ->
-        clearTimeout t
-        timer <- None
-    | _ -> ()
+        let clear () =
+            update JS.undefined (ResizeArray())
 
-let private selectionChanged (event : TextEditorSelectionChangeEvent) =
-    clearTimer()
-    timer <- Some (setTimeout (fun () -> StatusDisplay.update event.textEditor event.selections) 500.)
+    let mutable private timer = None
+    let private clearTimer () =
+        match timer with
+        | Some t ->
+            clearTimeout t
+            timer <- None
+        | _ -> ()
 
-let private textEditorChanged (_textEditor : TextEditor) =
-    clearTimer()
-    // The display is always cleared, if it's an F# document an onDocumentParsed event will arrive
-    StatusDisplay.clear()
-
-let private documentParsedHandler (event: Errors.DocumentParsedEvent) =
-    if event.document = window.activeTextEditor.document then
+    let private selectionChanged (event : TextEditorSelectionChangeEvent) =
         clearTimer()
-        StatusDisplay.update window.activeTextEditor window.activeTextEditor.selections
-    ()
+        timer <- Some (setTimeout (fun () -> StatusDisplay.update event.textEditor event.selections) 500.)
 
-let activate (context: ExtensionContext) =
-    StatusDisplay.activate context
+    let private textEditorChanged (_textEditor : TextEditor) =
+        clearTimer()
+        // The display is always cleared, if it's an F# document an onDocumentParsed event will arrive
+        StatusDisplay.clear()
 
-    context.subscriptions.Add(window.onDidChangeTextEditorSelection.Invoke(unbox selectionChanged))
-    context.subscriptions.Add(window.onDidChangeActiveTextEditor.Invoke(unbox textEditorChanged))
-    context.subscriptions.Add(Errors.onDocumentParsed.Invoke(unbox documentParsedHandler))
+    let private documentParsedHandler (event : Errors.DocumentParsedEvent) =
+        if event.document = window.activeTextEditor.document then
+            clearTimer()
+            StatusDisplay.update window.activeTextEditor window.activeTextEditor.selections
+        ()
+
+
+    let activate (context : ExtensionContext) =
+        StatusDisplay.activate context
+
+        context.subscriptions.Add(window.onDidChangeTextEditorSelection.Invoke(unbox selectionChanged))
+        context.subscriptions.Add(window.onDidChangeActiveTextEditor.Invoke(unbox textEditorChanged))
+        context.subscriptions.Add(Errors.onDocumentParsed.Invoke(unbox documentParsedHandler))
