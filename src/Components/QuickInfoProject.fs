870adca46100637051f385987a90b28a0d03e1f4
selketjah <gien@eightpointsquared.com>
2018-08-26 16:09:33 +0200

    clean up function declarations for quickinfoproject file

--- src/Components/QuickInfoProject.fs
+++ src/Components/QuickInfoProject.fs
@@ -1,40 +1,42 @@
-module Ionide.VSCode.FSharp.QuickInfoProject
+namespace Ionide.VSCode.FSharp
 
 open Fable.Import.vscode
 open Fable.Import.Node
 
 module node = Fable.Import.Node.Exports
 
-let mutable private item : StatusBarItem option = None
-let private hideItem () = item |> Option.iter (fun n -> n.hide ())
+module QuickInfoProject =
 
-let mutable projectPath = ""
+    let mutable private item : StatusBarItem option = None
+    let private hideItem () = item |> Option.iter (fun n -> n.hide ())
 
-let handler (te : TextEditor) =
-    hideItem ()
-    if te <> undefined && te.document <> undefined then
-        let path = te.document.fileName
-        let proj = Project.tryFindLoadedProjectByFile path
-        match proj with
-        | None -> ()
-        | Some p ->
-            projectPath <- p.Project
-            let pPath = node.path.basename p.Project
-            let text = sprintf "$(circuit-board) %s" pPath
-            item.Value.text <- text
-            item.Value.tooltip <- p.Project
-            item.Value.command <- "openProjectFileFromStatusbar"
-            item.Value.show()
+    let mutable projectPath = ""
 
-let handlerCommand() =
-    commands.executeCommand("vscode.open", Uri.file projectPath)
+    let handler (te : TextEditor) =
+        hideItem ()
+        if te <> undefined && te.document <> undefined then
+            let path = te.document.fileName
+            let proj = Project.tryFindLoadedProjectByFile path
+            match proj with
+            | None -> ()
+            | Some p ->
+                projectPath <- p.Project
+                let pPath = node.path.basename p.Project
+                let text = sprintf "$(circuit-board) %s" pPath
+                item.Value.text <- text
+                item.Value.tooltip <- p.Project
+                item.Value.command <- "openProjectFileFromStatusbar"
+                item.Value.show()
+
+    let handlerCommand() =
+        commands.executeCommand("vscode.open", Uri.file projectPath)
 
 
-let activate (context: ExtensionContext) =
-    commands.registerCommand("openProjectFileFromStatusbar", unbox<System.Func<obj,obj>> (fun _ -> handlerCommand))
-    |> context.subscriptions.Add
-    item <- Some (window.createStatusBarItem (StatusBarAlignment.Right, 10000. ))
-    context.subscriptions.Add(item.Value)
-    window.onDidChangeActiveTextEditor.Invoke(unbox handler) |> context.subscriptions.Add
-    if window.visibleTextEditors.Count > 0 then
-        handler window.visibleTextEditors.[0]
+    let activate (context : ExtensionContext) =
+        commands.registerCommand("openProjectFileFromStatusbar", unbox<System.Func<obj,obj>> (fun _ -> handlerCommand))
+        |> context.subscriptions.Add
+        item <- Some (window.createStatusBarItem (StatusBarAlignment.Right, 10000. ))
+        context.subscriptions.Add(item.Value)
+        window.onDidChangeActiveTextEditor.Invoke(unbox handler) |> context.subscriptions.Add
+        if window.visibleTextEditors.Count > 0 then
+            handler window.visibleTextEditors.[0]
