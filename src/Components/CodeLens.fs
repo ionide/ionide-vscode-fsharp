01e6c2dd4086aa095c2b8bc39b95b3affd2b3b7a
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2018-09-02 18:26:42 +0200

    Add CodeLenses for reference numbers (#910)

--- src/Components/CodeLens.fs
+++ src/Components/CodeLens.fs
@@ -13,7 +13,7 @@ module CodeLens =
 
     let refresh = EventEmitter<int>()
     let mutable private version = 0
-    let mutable private replacedByLineLens = false    
+    let mutable private replacedByLineLens = false
 
     let formatSignature (sign : SignatureData) : string =
         let formatType =
@@ -112,7 +112,87 @@ module CodeLens =
             member __.onDidChangeCodeLenses = EventEmitter().event
         }
 
-    let configChangedHandler () = 
+    let private createReferencesLensProvider () =
+        let isActive () =
+            let symbolCache = "FSharp.enableBackgroundSymbolCache" |> Configuration.get false
+            let referencesCodeLenses = "FSharp.enableReferenceCodeLens" |> Configuration.get false
+            symbolCache && referencesCodeLenses
+
+        let symbolsToCodeLens (doc : TextDocument) (symbols : Symbols[]) : CodeLens[] =
+            interestingSymbolPositions symbols
+                |> Array.map (CodeRange.fromDTO >> CodeLens)
+
+        { new CodeLensProvider with
+            member __.provideCodeLenses(doc, _) =
+                promise {
+                    let isActive = isActive ()
+                    if not isActive then
+                        return ResizeArray [||]
+                    else
+                        let text = doc.getText()
+                        let! result = LanguageService.declarations doc.fileName text version
+                        let data =
+                            if isNotNull result then
+                                let res = symbolsToCodeLens doc result.Data
+                                res
+                            else [||]
+                        return ResizeArray data
+                }
+                |> U2.Case2
+
+            member __.resolveCodeLens(codeLens, _) =
+                let load () =
+                    promise {
+                        let! (signaturesResult : SymbolUseResult) =
+                            LanguageService.symbolUseProject
+                                window.activeTextEditor.document.fileName
+                                (int codeLens.range.start.line + 1)
+                                (int codeLens.range.start.character + 1)
+                        let cmd = createEmpty<Command>
+                        cmd.title <-
+                            if isNotNull signaturesResult then
+                                if signaturesResult.Data.Uses.Length - 1 = 1 then "1 Reference"
+                                else sprintf "%d References" (signaturesResult.Data.Uses.Length - 1)
+                            else ""
+                        let locations =
+                            signaturesResult.Data.Uses |> Array.map (fun f ->
+                                let range =  CodeRange.fromSymbolUse f
+                                let uri = vscode.Uri.file f.FileName
+                                Location(uri, !!range)
+                            ) |> Array.toSeq |> ResizeArray
+                        let cmd =
+                            if signaturesResult.Data.Uses.Length > 1 then
+                                cmd.command <- "editor.action.showReferences"
+                                cmd.arguments <- [!!vscode.Uri.file(window.activeTextEditor.document.fileName); !!codeLens.range.start; !!locations ] |> List.toSeq |> ResizeArray |> Some
+                                cmd
+                            else cmd
+
+                        codeLens.command <- cmd
+                        return codeLens
+
+                    }
+
+                if int window.activeTextEditor.document.version > version then
+                    Promise.create (fun resolve error ->
+                        let mutable disp : Disposable option = None
+                        let d = refresh.event.Invoke(unbox (fun n ->
+                            resolve ()
+                            disp |> Option.iter (fun n -> n.dispose () |> ignore)))
+                        disp <- Some d
+
+                    )
+                    |> Promise.bind (fun _ ->
+                        load ()
+                    )
+                    |> U2.Case2
+
+                else
+                    load () |> U2.Case2
+
+            member __.onDidChangeCodeLenses = EventEmitter().event
+        }
+
+    let configChangedHandler () =
         let cfg = workspace.getConfiguration()
         replacedByLineLens <- (cfg.get("FSharp.lineLens.enabled", "never").ToLowerInvariant()) = "replacecodelens"
 
@@ -121,6 +201,7 @@ module CodeLens =
         workspace.onDidChangeConfiguration $ (configChangedHandler, (), context.subscriptions) |> ignore
         refresh.event.Invoke(fun n -> (version <- n ) |> unbox) |> context.subscriptions.Add
         languages.registerCodeLensProvider(selector, createProvider()) |> context.subscriptions.Add
+        languages.registerCodeLensProvider(selector, createReferencesLensProvider ()) |> context.subscriptions.Add
 
         configChangedHandler()
         ()
