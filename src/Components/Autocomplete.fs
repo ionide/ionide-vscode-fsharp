36beea54b73b280bdaf48e11dc1b95baaf0cd8d5
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2018-10-12 18:25:01 +0200

    Add autocomplete for # directives

--- src/Components/Autocomplete.fs
+++ src/Components/Autocomplete.fs
@@ -59,6 +59,30 @@ module Autocomplete =
                     sug.additionalTextEdits <- [| te |]
             sug
 
+        let hashDirectives =
+            ["r", "References an assembly"
+             "load", "Reads a source file, compiles it, and runs it."
+             "I", "Specifies an assembly search path in quotation marks."
+             "light", "Enables or disables lightweight syntax, for compatibility with other versions of ML"
+             "if", "Supports conditional compilation"
+             "else", "Supports conditional compilation"
+             "endif", "Supports conditional compilation"
+             "nowarn", "Disables a compiler warning or warnings"
+             "line", "Indicates the original source code line"]
+
+        let hashCompletions =
+            hashDirectives
+            |> List.map (fun (n,d) ->
+                let result = createEmpty<CompletionItem>
+                result.kind <- CompletionItemKind.Keyword
+                result.label <- "#" + n
+                result.insertText <- n
+                result.filterText <- n
+                result.sortText <- n
+                result.documentation <- d |> Markdown.createCommentBlock |> U2.Case2
+                result
+            ) |> ResizeArray
+
         { new CompletionItemProvider
           with
             member __.provideCompletionItems(doc, pos, _) =
@@ -66,14 +90,24 @@ module Autocomplete =
                     let setting = "FSharp.keywordsAutocomplete" |> Configuration.get true
                     let external = "FSharp.externalAutocomplete" |> Configuration.get true
                     let ln = doc.lineAt pos.line
-                    let! res = LanguageService.completion (doc.fileName) ln.text (int pos.line + 1) (int pos.character + 1) setting external (int doc.version)
-                    return mapCompletion res
+                    let r = doc.getWordRangeAtPosition pos
+                    let word = doc.getText r
+                    printfn "WORD: %s" word
+                    if not (ln.text.StartsWith "#" && (hashDirectives |> List.exists (fun (n,_) -> n.StartsWith word ) || word.Contains "\n" ))  then
+                        let! res = LanguageService.completion (doc.fileName) ln.text (int pos.line + 1) (int pos.character + 1) setting external (int doc.version)
+                        return mapCompletion res
+                    else
+                        printfn "Return hash completions"
+                        return hashCompletions
                 } |> U2.Case2
 
             member __.resolveCompletionItem(sug, _) =
                 promise {
-                    let! res = LanguageService.helptext sug.filterText
-                    return mapHelptext sug res
+                    if JS.isDefined sug.documentation then
+                        return sug
+                    else
+                        let! res = LanguageService.helptext sug.filterText
+                        return mapHelptext sug res
                 } |> U2.Case2
             }
 
