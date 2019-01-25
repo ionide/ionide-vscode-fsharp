2a3786c8a8c6f96c69291c54b192b67acf59d01f
Maxime Mangel <maxime.shadaen@protonmail.com>
2019-01-25 16:42:29 +0100

    Fix #921: Add smart indent support

--- src/Components/LanguageConfiguration.fs
+++ src/Components/LanguageConfiguration.fs
@@ -7,7 +7,17 @@ open Ionide.VSCode.Helpers
 
 module LanguageConfiguration =
 
-    let activate (context : ExtensionContext) =
+    let mutable private reference : Disposable option = None
+
+    let indentationRules =
+        jsOptions<IndentationRule> (fun o ->
+            o.increaseIndentPattern <- JS.RegExp.Create("""^(\s*(module|type|let|static member|member)\b.*=\s*)$|^(\s*(with get|and set)\b.*=.*)$|^(\s*(if|elif|then|else|static member|member)).*$""")
+
+            o.decreaseIndentPattern <- JS.RegExp.Create("""^(\s*(else|elif|and)).*$""")
+        )
+
+    let setLanguageConfiguration (context : ExtensionContext) =
+        // Config always setted
         let config =
             jsOptions<LanguageConfiguration> (fun o ->
                 o.onEnterRules <- Some <| ResizeArray<OnEnterRule>(
@@ -26,6 +36,36 @@ module LanguageConfiguration =
                 )
             )
 
-        JS.console.log config.onEnterRules.Value.[0].beforeText
+        let activateSmartIndent = "FSharp.smartIndent" |> Configuration.get false
 
-        context.subscriptions.Add(vscode.languages.setLanguageConfiguration("fsharp", config))
+        if activateSmartIndent then
+            config.indentationRules <- Some indentationRules
+
+        let triggerNotification =
+            match reference with
+            | Some oldReference ->
+                // Disable previous language configuration if there was one
+                oldReference.dispose() |> ignore
+                true
+            | None ->
+                false
+
+        reference <- Some <| vscode.languages.setLanguageConfiguration("fsharp", config)
+
+        // Notify the user if needed
+        if triggerNotification then
+            let msg =
+                if activateSmartIndent then
+                    "Smart indent has been activated for F#"
+                else
+                    "Smart indent has been deactivated for F#"
+
+            vscode.window.showInformationMessage(msg) |> ignore
+
+        context.subscriptions.Add(reference)
+
+    let activate (context : ExtensionContext) =
+        // We listen for config change so we can update on the fly the language configuration
+        workspace.onDidChangeConfiguration $ (setLanguageConfiguration, context, context.subscriptions) |> ignore
+
+        setLanguageConfiguration context
