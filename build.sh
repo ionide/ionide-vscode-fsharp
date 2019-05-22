1b63f87653f21f4b453db834003469c60d30082e
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2019-05-22 23:41:21 +0200

    Infrastructure

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ then
   packages/build/FAKE/tools/FAKE.exe $@ --nocache --fsiargs build.fsx
 else
   # use mono
-  mono ./paket.exe restore
+  mono ./.paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
   	exit $exit_code
