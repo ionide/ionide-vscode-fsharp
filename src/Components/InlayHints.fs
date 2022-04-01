514087dca87179cfe0b901ccf30d45af5d448ef6
Chet Husk <chusk3@gmail.com>
2022-03-31 19:59:03 -0500

    inlay hints support to go along with FSAC

---
+++ src/Components/InlayHints.fs
@@ -0,0 +1,59 @@
+module Ionide.VSCode.FSharp.InlayHints
+
+open Fable.Core
+open Fable.Import.VSCode
+open Fable.Import.VSCode.Vscode
+open global.Node
+open Fable.Core.JsInterop
+
+let private logger =
+    ConsoleAndOutputChannelLogger(Some "InlayHints", Level.DEBUG, None, Some Level.DEBUG)
+
+let toInlayHint (fsacHint: LanguageService.Types.InlayHint): InlayHint =
+    let h = createEmpty<InlayHint>
+    h.position <- vscode.Position.Create(fsacHint.pos.line, fsacHint.pos.character)
+    h.label <- U2.Case1 fsacHint.text
+    match fsacHint.kind with
+    | "Type" ->
+        h.paddingRight <- Some true
+        h.kind <- Some InlayHintKind.Type
+    | "Parameter" ->
+        h.paddingLeft <- Some true
+        h.kind <- Some InlayHintKind.Parameter
+    | _ -> ()
+    h
+
+let inlayProvider () =
+    let events = vscode.EventEmitter.Create()
+    let mutable ev = events.event
+    let dispose = workspace.onDidChangeTextDocument.Invoke(fun e ->
+        if e.document.languageId = "fsharp" then events.fire()
+        None
+    )
+    { new InlayHintsProvider with
+        member x.onDidChangeInlayHints = Some ev
+        member x.provideInlayHints(document: TextDocument, range: Range, token: CancellationToken) =
+            let doThing () =
+                promise {
+                    logger.Info ("Getting inlay hints for %s", document.fileName)
+                    let! hints = LanguageService.inlayHints(document.uri, { start = range.start; ``end`` = range.``end`` })
+                    return hints |> Seq.map toInlayHint |> ResizeArray |> Some
+                }
+                |> Promise.toThenable
+            ProviderResult.Some (U2.Case2 (doThing()))
+        member x.resolveInlayHint(hint, token: CancellationToken) = None
+        member x.onDidChangeInlayHints with set v =
+            v |> Option.iter (fun v -> ev <- v)
+        }, dispose
+
+let activate (context: ExtensionContext) =
+    let provider, dispose = inlayProvider()
+    let selector =
+        createObj [ "language" ==> "fsharp" ]
+        |> unbox<DocumentFilter>
+    languages.registerInlayHintsProvider(DocumentSelector.Case1 selector, provider)
+    |> context.Subscribe
+    context.Subscribe dispose
+
+    logger.Info "Activating F# inlay hints"
+    ()
--- src/fsharp.fs
+++ src/fsharp.fs
@@ -92,6 +92,7 @@ let activate (context: ExtensionContext) : JS.Promise<Api> =
         tryActivate "gitignore" Gitignore.activate context
         tryActivate "fsharpliterate" FSharpLiterate.activate context
         tryActivate "pipelinehints" PipelineHints.activate context
+        tryActivate "inlayhints" InlayHints.activate context
 
         let buildProject project =
             promise {
