22acf1eab90c6826e11fa3a13ea38af7392ac030
Chet Husk <chusk3@gmail.com>
2022-07-10 17:37:04 -0500

    minimal changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+//@ts-check
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
+
+'use strict';
+
 var path = require("path");
 
 function resolve(filePath) {
@@ -13,7 +18,7 @@ var babelOptions = {
   plugins: ["@babel/plugin-transform-runtime"]
 }
 
-module.exports = function(env, argv) {
+module.exports = function (env, argv) {
   var isProduction = argv.mode == "production"
   console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
 
@@ -21,40 +26,42 @@ module.exports = function(env, argv) {
   var outputPath = ionideExperimental ? "release-exp" : "release";
   console.log("Output path: " + outputPath);
 
-  return {
-  target: 'node',
-  mode: isProduction ? "production" : "development",
-  devtool: "source-map",
-  entry: './out/fsharp.js',
-  output: {
-    filename: 'fsharp.js',
-    path: resolve('./' + outputPath),
-    //library: 'IONIDEFSHARP',
-    libraryTarget: 'commonjs2'
-  },
-  resolve: {
-    modules: [resolve("./node_modules/")]
-  },
-  //externals: [nodeExternals()],
-  externals: {
-    // Who came first the host or the plugin ?
-    "vscode": "commonjs vscode",
+  /**@type WebpackConfig*/
+  const config = {
+    target: 'node',
+    mode: isProduction ? "production" : "development",
+    devtool: "source-map",
+    entry: './out/fsharp.js',
+    output: {
+      filename: 'fsharp.js',
+      path: resolve('./' + outputPath),
+      //library: 'IONIDEFSHARP',
+      libraryTarget: 'commonjs2'
+    },
+    resolve: {
+      modules: [resolve("./node_modules/")]
+    },
+    //externals: [nodeExternals()],
+    externals: {
+      // Who came first the host or the plugin ?
+      "vscode": "commonjs vscode",
 
-    // Optional dependencies of ws
-    "utf-8-validate": "commonjs utf-8-validate",
-    "bufferutil": "commonjs bufferutil"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: babelOptions
-        },
-      }
-    ]
-  }
-};
+      // Optional dependencies of ws
+      "utf-8-validate": "commonjs utf-8-validate",
+      "bufferutil": "commonjs bufferutil"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: babelOptions
+          },
+        }
+      ]
+    }
+  };
+  return config;
 }
