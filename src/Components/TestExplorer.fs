60d7bb248c7b6ba5c6f9b0e6a87e77de3fa4b1b3
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2023-09-18 11:13:23 -0500

    Fix regression of AutoDiscoverTestsOnLoad behavior (#1933)
    Conditional was removed unintentionally. Perhaps clobbered in a merge?
    https://github.com/ionide/ionide-vscode-fsharp/commit/61c4d71ef5b255a77aa368548530380174d39ac4#r127656246

--- src/Components/TestExplorer.fs
+++ src/Components/TestExplorer.fs
@@ -1714,10 +1714,13 @@ let activate (context: ExtensionContext) =
 
     testController.refreshHandler <- Some refreshHandler
 
+    let shouldAutoDiscoverTests =
+        Configuration.get true "FSharp.TestExplorer.AutoDiscoverTestsOnLoad"
+
     let mutable hasInitiatedDiscovery = false
 
     Project.workspaceLoaded.Invoke(fun () ->
-        if not hasInitiatedDiscovery then
+        if shouldAutoDiscoverTests && not hasInitiatedDiscovery then
             hasInitiatedDiscovery <- true
 
             let trxTests =
